62afa2a744c78b8796b66f4da953290655d94c1b
nfdvv <nefedoff.vadim@gmail.com>
2022-09-29 20:33:32 +0300

    change slider

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ $(function(){
     slidesToShow: 5,
     slidesToScroll: 1,
     autoplay: true,
-    autoplaySpeed: 3000,
+    // autoplaySpeed: 3000,
     arrows: true,
     prevArrow: '<button type="button" class="slick-prev"><img src="images/arrow-prev-circle.svg" alt="arrow-prev"></button>',
     nextArrow: '<button type="button" class="slick-next"><img src="images/arrow-next-circle.svg" alt="arrow-next"></button>',
