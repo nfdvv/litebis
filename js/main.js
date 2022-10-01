96aa9f9b11a0cf70f286e4cbd675ef9e9ac652ab
nfdvv <nefedoff.vadim@gmail.com>
2022-10-01 18:15:08 +0300

    added changes to the news slider

--- js/main.js
+++ js/main.js
@@ -8,9 +8,9 @@ $(function(){
   $('.news__inner').slick({
     slidesToShow: 5,
     slidesToScroll: 1,
-    // autoplay: true,
-    // autoplaySpeed: 3000,
-    // arrows: true,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    arrows: true,
     prevArrow: '<button type="button" class="slick-prev"><img src="images/arrow-prev-circle.svg" alt="arrow-prev"></button>',
     nextArrow: '<button type="button" class="slick-next"><img src="images/arrow-next-circle.svg" alt="arrow-next"></button>',
   });
