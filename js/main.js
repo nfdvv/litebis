7b56798d00d6b4bb08ba4f2a63f1d88bf4cf729b
nfdvv <nefedoff.vadim@gmail.com>
2022-10-05 23:14:35 +0300

    finished media

--- js/main.js
+++ js/main.js
@@ -13,6 +13,26 @@ $(function(){
     arrows: true,
     prevArrow: '<button type="button" class="slick-prev"><img src="images/arrow-prev-circle.svg" alt="arrow-prev"></button>',
     nextArrow: '<button type="button" class="slick-next"><img src="images/arrow-next-circle.svg" alt="arrow-next"></button>',
+    responsive: [
+      {
+        breakpoint: 1120,
+        settings: {
+          slidesToShow: 3,
+        }
+      },
+      {
+        breakpoint: 740,
+        settings: {
+          slidesToShow: 2,
+        }
+      },
+      {
+        breakpoint: 510,
+        settings: {
+          slidesToShow: 1,
+        }
+      },
+    ]
   });
   $('.header__menu-btn').on('click', function(){
     $('.header__menu > ul').slideToggle();
